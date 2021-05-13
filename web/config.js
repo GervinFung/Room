b9c30e80c99f6a8862e7f5dec9a829b4a9ca9077
PoolOfDeath20 <gervinfungdaxuen@gmail.com>
2021-05-14 01:20:35 +0800

    Made some changes everywhere

--- web/config.js
+++ web/config.js
@@ -1,12 +1,12 @@
-var username = "eugeneyjy";
-var password = "Dnthackmepls78";
-var host = "room.88id4.mongodb.net";
-var dbname = "utar_accom";
-var dbparam = "retryWrites=true&w=majority";
-var collection = "room";
+const username = 'eugeneyjy'
+const password = 'Dnthackmepls78'
+const host = 'room.88id4.mongodb.net'
+const dbname = 'utar_accom'
+const dbparam = 'retryWrites=true&w=majority'
+const collection = 'room'
 
-var url = `mongodb+srv://${username}:${password}@${host}/${dbname}?${dbparam}`;
+const url = `mongodb+srv://${username}:${password}@${host}/${dbname}?${dbparam}`
 
-exports.url = url;
-exports.dbname = dbname;
-exports.collection = collection;
+exports.url = url
+exports.dbname = dbname
+exports.collection = collection
--- web/server.js
+++ web/server.js
@@ -1,48 +1,48 @@
-var path = require('path');
-var express = require('express');
-var exphbs = require('express-handlebars');
-var bodyParser = require('body-parser');
-var MongoClient = require('mongodb').MongoClient;
-var config = require('./config.js');
+const path = require('path')
+const express = require('express')
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient
+const config = require('./config.js')
+const
+const app = express()
+const port = 8000
 
-var app = express();
-var port = 8000;
+let db = null
 
-var db = null;
+app.engine('handlebars', exphbs({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
 
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
+app.use(express.static('public'))
 
-app.use(express.static('public'));
+app.use(bodyParser.json())
 
-app.use(bodyParser.json());
+app.get('/', (req, res, next) => {
+  res.status(200).sendFile(path.resolve('./public/html/index.html'))
+})
 
-app.get('/', function(req, res, next){
-  res.status(200).sendFile(path.resolve('./public/html/index.html'));
-});
-
-app.get('/room', function(req, res, next){
-  var campus = req.query.campus;
-  var collection = db.collection(config.collection);
-  collection.find({campus: campus}).toArray(function(err, places) {
+app.get('/room', (req, res, next) => {
+  const campus = req.query.campus
+  const collection = db.collection(config.collection)
+  collection.find({campus: campus}).toArray((err, places) => {
     if(err) {
       res.status(500).send({
-        error: "Error fetching campus from database"
-      });
+        error: 'Error fetching campus from database'
+      })
     } else {
-      res.status(200).render('roomPage', {places});
+      res.status(200).render('roomPage', {places})
     }
   })
-});
+})
 
-MongoClient.connect(config.url, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, client) {
+MongoClient.connect(config.url, {useNewUrlParser: true, useUnifiedTopology: true},  (err, client) => {
   if(err) {
-    console.error("Failed to connect database");
-    throw err;
+    console.error('Failed to connect database')
+    throw err
   }
-  db = client.db(config.dbname);
-  app.listen(port, function() {
-    console.log("===Server listening on port ", port);
-  });
-  var collection = db.collection(config.collection);
-});
+  db = client.db(config.dbname)
+  app.listen(port, () => {
+    console.log('===Server listening on port ', port)
+  })
+  const collection = db.collection(config.collection)
+})
